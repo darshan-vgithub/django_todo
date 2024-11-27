5e2e95935275031dab0bad737a32da609f22de44
Darshan.V <darshanv487@gmail.com>
2024-11-27 15:50:55 +0530

    created the views and working on the templates now

--- todo_project/todo/views.py
+++ todo_project/todo/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Todo
 
-# Create your views here.
+def todo_list(request):
+    todos = Todo.objects.all()
+    return render(request, 'todo/todo_list.html', {'todos': todos})
+
+def todo_add(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        description = request.POST['description']
+        Todo.objects.create(title=title, description=description)
+        return redirect('todo_list')
+    return render(request, 'todo/todo_add.html')
+
+def todo_edit(request, id):
+    todo = get_object_or_404(Todo, id=id)
+    if request.method == 'POST':
+        todo.title = request.POST['title']
+        todo.description = request.POST['description']
+        todo.completed = 'completed' in request.POST
+        todo.save()
+        return redirect('todo_list')
+    return render(request, 'todo/todo_edit.html', {'todo': todo})
+
+def todo_delete(request, id):
+    todo = get_object_or_404(Todo, id=id)
+    if request.method == 'POST':
+        todo.delete()
+        return redirect('todo_list')
+    return render(request, 'todo/todo_delete.html', {'todo': todo})
